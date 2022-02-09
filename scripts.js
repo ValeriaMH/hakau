1a200e34c509cf104d68aacc47684fb732a5f29a
Valeria <vmoraleshuaman@gmail.com>
2022-02-09 11:46:21 +1030

    adding front matter

--- scripts.js
+++ scripts.js
@@ -162,22 +162,40 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 // COUNTER
-const counterUp = window.counterUp.default
+// const counterUp = window.counterUp.default
 
-const callback = entries => {
-	entries.forEach( entry => {
-		const el = entry.target
-		if ( entry.isIntersecting && ! el.classList.contains( 'is-visible' ) ) {
-			counterUp( el, {
-				duration: 2000,
-				delay: 16,
-			} )
-			el.classList.add( 'is-visible' )
-		}
-	} )
-}
+// const callback = entries => {
+// 	entries.forEach( entry => {
+// 		const el = entry.target
+// 		if ( entry.isIntersecting && ! el.classList.contains( 'is-visible' ) ) {
+// 			counterUp( el, {
+// 				duration: 2000,
+// 				delay: 16,
+// 			} )
+// 			el.classList.add( 'is-visible' )
+// 		}
+// 	} )
+// }
 
-const IO = new IntersectionObserver( callback, { threshold: 1 } )
+// const IO = new IntersectionObserver( callback, { threshold: 1 } )
 
-const el = document.querySelector( '.counter' )
-IO.observe( el )
+// const el = document.querySelector( '.counter' )
+// IO.observe( el )
+
+$(".counter").each(function () {
+  $(this)
+    .prop("Counter", 0)
+    .animate(
+      {
+        Counter: $(this).text(),
+      },
+      {
+        duration: 2000,
+        easing: "swing",
+        step: function (now) {
+          now = Number(Math.ceil(now)).toLocaleString('en');
+                                $(this).text(now);
+        },
+      }
+    );
+});
