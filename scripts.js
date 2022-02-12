b27bf5003b9ca40da88791ffabee49f0a174143a
Valeria <vmoraleshuaman@gmail.com>
2022-02-12 16:08:00 +1030

    contact ok

--- scripts.js
+++ scripts.js
@@ -207,3 +207,18 @@ lightGallery(document.getElementById('lightgallery'), {
   
 });
 
+// Accordion FAQ
+
+var allPanels = $('.accordion > dd').hide();
+
+$('.accordion > dt').click(function() {
+  allPanels.slideUp();
+
+  $(this).filter('.open').removeClass('open').addClass('closing')
+  $('.accordion > dt ').removeClass('open');
+  $(this).not(".closing").addClass('open').next().slideDown();
+  $('.accordion > dt').removeClass('closing');
+  
+  return false;
+});
+
