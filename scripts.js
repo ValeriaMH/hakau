fa8c4b7ed66b5652949ae0213a7a969bbf0115d1
Valeria <vmoraleshuaman@gmail.com>
2022-05-31 20:36:01 +0930

    x

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     $(this).toggleClass('active');
   });
 
-   // COURSES TYPE
+   // ALLIES STORIES
 
 const tabs = document.querySelectorAll('.allies-stories__tab');
 const tabsContainer = document.querySelector('.allies-stories__tab-container');
@@ -29,6 +29,52 @@ tabsContainer.addEventListener('click', function (e) {
   document
     .querySelector(`.allies-stories__content--${clicked.dataset.tab}`)
     .classList.add('allies-stories__content--active');
+});
+
+
+// Allies Slider
+
+$('.allies-slider-container').slick({
+  
+  autoplay: true,
+  autoplaySpeed: 3000,
+  speed: 500,
+  arrows: true,
+  accesibility: true,
+  dots: false,
+  fade: false,
+  infinite: true,
+  pauseOnHover: true,
+  pauseOnDotsHover: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+  
 });
 
 
@@ -85,6 +131,8 @@ $('.testimonial-slider-container').slick({
   slidesToScroll: 2
 
 });
+
+
 
 //  Image Lazy loading
 document.addEventListener("DOMContentLoaded", function () {
